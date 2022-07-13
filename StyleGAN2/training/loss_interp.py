c29521f17b64c617ffa1137db9831084c5c1640e
dzld00 <38360844+dzld00@users.noreply.github.com>
2022-07-13 11:39:37 -0700

    Add files via upload

---
+++ StyleGAN2/training/loss_interp.py
@@ -0,0 +1,380 @@
+# Copyright (c) 2021, NVIDIA CORPORATION.  All rights reserved.
+#
+# NVIDIA CORPORATION and its licensors retain all intellectual property
+# and proprietary rights in and to this software, related documentation
+# and any modifications thereto.  Any use, reproduction, disclosure or
+# distribution of this software and related documentation without an express
+# license agreement from NVIDIA CORPORATION is strictly prohibited.
+
+import numpy as np
+import torch
+from torch_utils import training_stats
+from torch_utils import misc
+from torch_utils.ops import conv2d_gradfix
+
+from torch import nn
+import torch.nn.functional as F
+from math import exp
+from sklearn.manifold import MDS
+import random
+
+#----------------------------------------------------------------------------
+
+class Loss:
+    def accumulate_gradients(self, phase, real_img, real_c, gen_z, gen_c, sync, gain): # to be overridden by subclass
+        raise NotImplementedError()
+
+#----------------------------------------------------------------------------
+
+class StyleGAN2Loss(Loss):
+    def __init__(self, device, G_mapping, G_synthesis, D, augment_pipe=None, style_mixing_prob=0.9, r1_gamma=10, pl_batch_shrink=2, pl_decay=0.01, pl_weight=2):
+        super().__init__()
+        self.device = device
+        self.G_mapping = G_mapping
+        self.G_synthesis = G_synthesis
+        self.D = D
+        self.augment_pipe = augment_pipe
+        self.style_mixing_prob = style_mixing_prob
+        self.r1_gamma = r1_gamma
+        self.pl_batch_shrink = pl_batch_shrink
+        self.pl_decay = pl_decay
+        self.pl_weight = pl_weight
+        self.pl_mean = torch.zeros([], device=device)
+
+    def run_G(self, z, c, sync):
+        with misc.ddp_sync(self.G_mapping, sync):
+            ws = self.G_mapping(z, c)
+            if self.style_mixing_prob > 0:
+                with torch.autograd.profiler.record_function('style_mixing'):
+                    cutoff = torch.empty([], dtype=torch.int64, device=ws.device).random_(1, ws.shape[1])
+                    cutoff = torch.where(torch.rand([], device=ws.device) < self.style_mixing_prob, cutoff, torch.full_like(cutoff, ws.shape[1]))
+                    ws[:, cutoff:] = self.G_mapping(torch.randn_like(z), c, skip_w_avg_update=True)[:, cutoff:]
+        with misc.ddp_sync(self.G_synthesis, sync):
+            img = self.G_synthesis(ws)
+        return img, ws
+
+    def run_D(self, img, c, sync):
+        if self.augment_pipe is not None:
+            img = self.augment_pipe(img)
+        with misc.ddp_sync(self.D, sync):
+            logits = self.D(img, c)
+        return logits
+
+    def accumulate_gradients(self, phase, real_img, real_c, gen_z, gen_c, sync, gain):
+        assert phase in ['Gmain', 'Greg', 'Gboth', 'Dmain', 'Dreg', 'Dboth']
+        do_Gmain = (phase in ['Gmain', 'Gboth'])
+        do_Dmain = (phase in ['Dmain', 'Dboth'])
+        do_Gpl   = (phase in ['Greg', 'Gboth']) and (self.pl_weight != 0)
+        do_Dr1   = (phase in ['Dreg', 'Dboth']) and (self.r1_gamma != 0)
+
+        augment_prob, kk = 0, 0
+        # Gmain: Maximize logits for generated images.
+        if do_Gmain:
+            with torch.autograd.profiler.record_function('Gmain_forward'):
+                gen_img, _gen_ws = self.run_G(gen_z, gen_c, sync=(sync and not do_Gpl)) # May get synced by Gpl.
+                gen_logits = self.run_D(gen_img, gen_c, sync=False)
+                training_stats.report('Loss/scores/fake', gen_logits)
+                training_stats.report('Loss/signs/fake', gen_logits.sign())
+                #loss_Gmain = torch.nn.functional.softplus(-gen_logits) # -log(sigmoid(gen_logits))
+                augment_prob, kk = dynamic_prob(gen_logits)
+                #print(augment_prob, kk)
+                batch_size = gen_logits.size()[0]
+                gen_logits_aug = near_interp(gen_logits, kk, augment_prob)
+                #loss_Gmain = torch.nn.functional.softplus(-gen_logits_aug).mean()
+                loss_Gmain = torch.nn.functional.relu(-gen_logits_aug).mean()
+                training_stats.report('Loss/G/loss', loss_Gmain)
+            with torch.autograd.profiler.record_function('Gmain_backward'):
+                loss_Gmain.mean().mul(gain).backward()
+
+        # Gpl: Apply path length regularization.
+        if do_Gpl:
+            with torch.autograd.profiler.record_function('Gpl_forward'):
+                batch_size = gen_z.shape[0] // self.pl_batch_shrink
+                gen_img, gen_ws = self.run_G(gen_z[:batch_size], gen_c[:batch_size], sync=sync)
+                pl_noise = torch.randn_like(gen_img) / np.sqrt(gen_img.shape[2] * gen_img.shape[3])
+                with torch.autograd.profiler.record_function('pl_grads'), conv2d_gradfix.no_weight_gradients():
+                    pl_grads = torch.autograd.grad(outputs=[(gen_img * pl_noise).sum()], inputs=[gen_ws], create_graph=True, only_inputs=True)[0]
+                pl_lengths = pl_grads.square().sum(2).mean(1).sqrt()
+                pl_mean = self.pl_mean.lerp(pl_lengths.mean(), self.pl_decay)
+                self.pl_mean.copy_(pl_mean.detach())
+                pl_penalty = (pl_lengths - pl_mean).square()
+                training_stats.report('Loss/pl_penalty', pl_penalty)
+                loss_Gpl = pl_penalty * self.pl_weight
+                training_stats.report('Loss/G/reg', loss_Gpl)
+            with torch.autograd.profiler.record_function('Gpl_backward'):
+                (gen_img[:, 0, 0, 0] * 0 + loss_Gpl).mean().mul(gain).backward()
+
+        # Dmain: Minimize logits for generated images.
+        loss_Dgen = 0
+        if do_Dmain:
+            with torch.autograd.profiler.record_function('Dgen_forward'):
+                gen_img, _gen_ws = self.run_G(gen_z, gen_c, sync=False)
+                gen_logits = self.run_D(gen_img, gen_c, sync=False) # Gets synced by loss_Dreal.
+                training_stats.report('Loss/scores/fake', gen_logits)
+                training_stats.report('Loss/signs/fake', gen_logits.sign())
+                #loss_Dgen = torch.nn.functional.softplus(gen_logits) # -log(1 - sigmoid(gen_logits))
+                augment_prob, kk = dynamic_prob(gen_logits)
+                #print(augment_prob, kk)
+                batch_size = gen_logits.size()[0]
+                gen_logits_aug = near_interp(gen_logits, kk, augment_prob)
+                #loss_Dgen = torch.nn.functional.softplus(gen_logits_aug).mean()
+                loss_Dgen = torch.nn.functional.relu(1 + gen_logits_aug).mean()
+            with torch.autograd.profiler.record_function('Dgen_backward'):
+                loss_Dgen.mean().mul(gain).backward()
+
+        # Dmain: Maximize logits for real images.
+        # Dr1: Apply R1 regularization.
+        if do_Dmain or do_Dr1:
+            name = 'Dreal_Dr1' if do_Dmain and do_Dr1 else 'Dreal' if do_Dmain else 'Dr1'
+            with torch.autograd.profiler.record_function(name + '_forward'):
+                real_img_tmp = real_img.detach().requires_grad_(do_Dr1)
+                real_logits = self.run_D(real_img_tmp, real_c, sync=sync)
+                training_stats.report('Loss/scores/real', real_logits)
+                training_stats.report('Loss/signs/real', real_logits.sign())
+
+                loss_Dreal = 0
+                if do_Dmain:
+                    #loss_Dreal = torch.nn.functional.softplus(-real_logits) # -log(sigmoid(real_logits))
+                    augment_prob, kk = dynamic_prob(real_logits)
+                    batch_size = real_logits.size()[0]
+                    #print(augment_prob, kk)
+                    real_logits_aug = near_interp(real_logits, kk, augment_prob)
+                    #loss_Dreal = torch.nn.functional.softplus(-real_logits_aug).mean()
+                    loss_Dreal = torch.nn.functional.relu(1-real_logits_aug).mean()
+                    training_stats.report('Loss/D/loss', loss_Dgen + loss_Dreal)
+
+                loss_Dr1 = 0
+                if do_Dr1:
+                    with torch.autograd.profiler.record_function('r1_grads'), conv2d_gradfix.no_weight_gradients():
+                        r1_grads = torch.autograd.grad(outputs=[real_logits.sum()], inputs=[real_img_tmp], create_graph=True, only_inputs=True)[0]
+                    r1_penalty = r1_grads.square().sum([1,2,3])
+                    loss_Dr1 = r1_penalty * (self.r1_gamma / 2)
+                    training_stats.report('Loss/r1_penalty', r1_penalty)
+                    training_stats.report('Loss/D/reg', loss_Dr1)
+
+            with torch.autograd.profiler.record_function(name + '_backward'):
+                #(real_logits * 0 + loss_Dreal + loss_Dr1).mean().mul(gain).backward()
+                (real_logits.mean() * 0 + loss_Dreal + loss_Dr1).mean().mul(gain).backward()
+                
+        return augment_prob, kk
+
+#----------------------------------------------------------------------------
+
+                
+class TripletLoss(nn.Module):
+    """
+    Triplet loss
+    Takes embeddings of an anchor sample, a positive sample and a negative sample
+    """
+
+    def __init__(self, margin, alpha):
+        super(TripletLoss, self).__init__()
+        self.margin = margin
+        self.alpha = alpha
+
+    def forward(self, anchor, positive, negative, size_average=True):    
+        
+        distance_positive = (anchor - positive).pow(2).sum(1)  # .pow(.5)
+        distance_negative = (anchor - negative).pow(2).sum(1)  # .pow(.5)
+        
+        cos = nn.CosineSimilarity(dim=1, eps=1e-6)
+        cos_reg = cos(negative - anchor, positive - anchor).sum(0) 
+        
+        #losses = F.relu(distance_positive - distance_negative + self.margin - self.alpha * cos_reg)
+        losses = F.softplus(distance_positive - distance_negative + self.margin - self.alpha * cos_reg)
+
+        return losses.mean()
+    
+
+def pairwise_distances(x, y):
+    '''
+    Input: x is a Nxd matrix
+           y is an optional Mxd matirx
+    Output: dist is a NxM matrix where dist[i,j] is the square norm between x[i,:] and y[j,:]
+            if y is not given then use 'y=x'.
+    i.e. dist[i,j] = ||x[i,:]-y[j,:]||^2
+    '''
+    x_norm = (x**2).sum(1).view(-1, 1)
+    if y is not None:
+        y_t = torch.transpose(y, 0, 1)
+        y_norm = (y**2).sum(1).view(1, -1)
+    else:
+        y_t = torch.transpose(x, 0, 1)
+        y_norm = x_norm.view(1, -1)
+    
+    dist = x_norm + y_norm - 2.0 * torch.mm(x, y_t)
+    # Ensure diagonal is zero if x=y
+    # if y is None:
+    #     dist = dist - torch.diag(dist.diag)
+    return torch.sqrt(torch.clamp(dist, 0.0, np.inf))        
+
+
+def near_interp(embeddings, k, augment_prob, s=0):
+    if k == 1 or augment_prob == 0:
+        return embeddings
+    
+    k = min(k, embeddings.size()[0])
+    
+    pd = pairwise_distances(embeddings, embeddings) 
+    
+    pd = pd/pd.max()
+    pd_s = (1 / (1+pd))#.detach().cpu().numpy()
+    #print(pd_s)
+    
+    k_smallest = torch.topk(pd, k, largest=False).indices
+    
+    #print('k_smallest: ', k_smallest.size())
+    #print(k_smallest)
+    
+    concat_embeddings = embeddings.clone()
+    
+    #t = (1-augment_prob)**s
+    #t = -np.log(augment_prob)*s
+    #t = (1-augment_prob)*s
+    t = 1
+    
+    #alpha = [1]*k #initialize
+    alpha = torch.ones(k, device=embeddings.device)
+    for row in k_smallest:
+        inner_pts = torch.zeros((1,embeddings.size()[1]), device=embeddings.device)
+        for i in range(k):
+            alpha[i] = pd_s[row[0],row[i]]**t
+        #print(row, alpha)
+        #q = np.random.dirichlet(alpha)
+        q = torch.distributions.dirichlet.Dirichlet(alpha).sample().to(embeddings.device)
+        #print(q)
+        for idx in range(k):   
+            inner_pts = inner_pts + embeddings[row[idx].unsqueeze(-1),:] * q[idx,None]
+        inner_pts = F.normalize(inner_pts)
+        concat_embeddings = torch.cat((concat_embeddings, inner_pts), dim=0)
+    #print(concat_embeddings.size())
+    
+    batch_size = embeddings.size()[0]
+    inner_embeddings = concat_embeddings[batch_size:,:]   
+    #print(inner_embeddings.size())
+    
+    out_embeddings = embeddings.clone()
+    for idx in range(embeddings.size()[0]):
+        p = random.random()
+        if p < augment_prob:
+            out_embeddings = torch.cat((out_embeddings, inner_embeddings[idx,:].unsqueeze(0)), dim=0)
+        else:
+            out_embeddings = torch.cat((out_embeddings, embeddings[idx,:].unsqueeze(0)), dim=0)
+    
+    samples = out_embeddings[batch_size:,:]
+    
+    return samples
+
+def dynamic_prob(embeddings, augment_type=6, r=1):
+    embeddings = F.normalize(embeddings)
+    batch_size = embeddings.size()[0] 
+    
+    D = pairwise_distances(embeddings, embeddings)   
+    D = D.detach().cpu().numpy()  
+    #D = np.sqrt(D)
+    D = D / np.amax(D)
+    
+    #l_sorted = cmdscale(D) 
+    l_sorted = eigen_mds(D)           
+    #l_sorted = eigen_pca(embeddings)
+    
+    #l_sorted = l_sorted[:3]
+    
+#     print()
+#     print(embeddings.size())
+#     print(D)
+#     print(l_sorted)
+    #print(np.var(l_sorted), np.mean(l_sorted))
+    
+    p = 0
+    k = batch_size - next(x[0] for x in enumerate(l_sorted) if x[1] < 0.1 * l_sorted[0]) #0.1,0.5
+    
+    if augment_type == 1:
+        p = np.mean(l_sorted) * np.var(l_sorted) * r
+        p = 1 / exp(p) 
+    
+    if augment_type == 2:
+        p = ((l_sorted[4] - l_sorted[7]) / (l_sorted[0] - l_sorted[7])) ** (l_sorted[0] * r)    
+        p = np.sin(p*1.57)     #p = exp( -5 * p) ; p = np.tanh(p)   
+        
+    if augment_type == 3:
+        #embeddings = F.normalize(embeddings)
+        p = torch.matmul(embeddings,torch.transpose(embeddings,0,1))
+        p = torch.det(p) * r
+        p = exp(p)
+      
+    if augment_type == 4:
+        #p = ((l_sorted[batch_size//2] - l_sorted[batch_size-1]) / (l_sorted[0] - l_sorted[batch_size-1])) ** (np.sum(l_sorted) * r)
+        p = (l_sorted[1]  / l_sorted[0]) ** (np.mean(l_sorted) * r)
+        
+       
+    if augment_type == 5:
+        p = exp(negative * r)
+        
+#     if augment_type == 6:
+#         p = (1 / exp(l_sorted[0]))**(np.mean(l_sorted) * r)
+#         #p = (1 / exp(l_sorted[0]))**(np.var(l_sorted) * r)
+    
+    p = (k-1) / batch_size
+    
+    #p = min(p, 0.9)
+    #k = 2
+    #p = 0
+    
+    return p, k  
+
+def cmdscale(D):
+    """                                                                                       
+    Classical multidimensional scaling (MDS)                                                  
+                                                                                               
+    Parameters                                                                                
+    ----------                                                                                
+    D : (n, n) array                                                                          
+        Symmetric distance matrix.                                                            
+                                                                                               
+    Returns                                                                                   
+    -------                                                                                   
+    Y : (n, p) array                                                                          
+        Configuration matrix. Each column represents a dimension. Only the                    
+        p dimensions corresponding to positive eigenvalues of B are returned.                 
+        Note that each dimension is only determined up to an overall sign,                    
+        corresponding to a reflection.                                                        
+                                                                                               
+    e : (n,) array                                                                            
+        Eigenvalues of B.                                                                     
+                                                                                               
+    """
+    
+    # Number of points                                                                        
+    n = len(D)
+ 
+    # Centering matrix                                                                        
+    H = np.eye(n) - np.ones((n, n))/n
+ 
+    # YY^T                                                                                    
+    B = -H.dot(D**2).dot(H)/2
+ 
+    # Diagonalize                                                                             
+    evals, evecs = np.linalg.eigh(B)
+ 
+    # Sort by eigenvalue in descending order                                                  
+    idx   = np.argsort(evals)[::-1]
+    evals = evals[idx]
+    evecs = evecs[:,idx]
+ 
+    # Compute the coordinates using positive-eigenvalued components only                      
+#     w, = np.where(evals > 0)
+#     L  = np.diag(np.sqrt(evals[w]))
+#     V  = evecs[:,w]
+#     Y  = V.dot(L)
+ 
+    return np.sort(evals)[::-1]
+
+def eigen_mds(pd):
+    mds = MDS(n_components=len(pd), dissimilarity='precomputed')
+    pts = mds.fit_transform(pd)
+    #print(pts.shape)
+    _,l_sorted,_ = np.linalg.svd(pts)
+    return l_sorted
+
+#----------------------------------------------------------------------------
